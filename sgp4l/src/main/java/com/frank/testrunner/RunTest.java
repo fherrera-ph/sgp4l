53007dd53aa8fee46761bff5dd6960f70618c726
frankjas <frankjas@franks-mbp>
2021-02-07 11:23:10 +0800

    test

---
+++ sgp4l/src/main/java/com/frank/testrunner/RunTest.java
@@ -0,0 +1,11 @@
+package com.frank.testrunner;
+
+public class RunTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+	}
+
+}
